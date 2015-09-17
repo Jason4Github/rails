52a823ee422e0882d58f774391800142adec9f95
jason_yu <yuhaishengyhs@gmail.com>
2015-09-17 06:26:08 +0000

    add repo, new page

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get 'static_pages/BP'
 
   get 'static_pages/IDMTOOLS'
+  
+  get 'static_pages/REPO'
   root 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
